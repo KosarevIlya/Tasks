81ca2b6eb37e0414b88b733d505969536048f890
unknown <Kosarevis.74@gmail.com>
2023-03-22 18:10:33 +0500

    загружены программы 00, 01, 02, 03, 05, 07

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,3 +3,13 @@
 // какое меньшее.
 
 
+Console.WriteLine("Введите первое число = ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Ведите второе число = ");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+if (a > b )  Console.WriteLine("Первое число больше второго");
+
+else if (b>a) Console.WriteLine("Второе число больше первого");
+else Console.WriteLine("Числа равны");
+    
