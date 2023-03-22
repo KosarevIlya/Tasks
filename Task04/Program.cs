aa13c6ab5ea55791828f4477b5024e73a70967d7
unknown <Kosarevis.74@gmail.com>
2023-03-22 18:47:40 +0500

    Поиск максимального из трех чисел

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на 
+// вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число = ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Ведите второе число = ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите третье число = ");
+int c = Convert.ToInt32 (Console.ReadLine());
+
+int max = a;
+if (a > b )  max = a;
+if (a > c ) max = a; 
+if (b > max ) max = b;
+if ( c > max) max = c;
+
+Console.WriteLine($"Максимальное из введеных чисел = {max}");
+
