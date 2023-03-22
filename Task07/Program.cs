d60353cea995d86324dcdfe043fba55ffb1cdb4d
unknown <Kosarevis.74@gmail.com>
2023-03-22 15:24:05 +0500

    Добавили решение 7 задачи

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число = ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * -1;
+}
+if (number >= 100 && number <= 999)
+
+
+{
+    int lastnumber = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number} --> {lastnumber}");
+}
+ else Console.WriteLine ("Вы вышли за пределы оговоренного задания");
