23229c236a01e3403c766c07868786166e65f801
unknown <Kosarevis.74@gmail.com>
2023-03-23 14:41:21 +0500

    добавлена 14 задача

---
+++ Task14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принять число и проверить, кратно ли оно 23 и 7
+
+
+Console.WriteLine(" Введите число=");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine (Miltiple(number) ? "да" : "нет"); 
+bool Miltiple (int num)
+
+{
+    return num % 7 == 0 & num % 23 == 0;
+}
