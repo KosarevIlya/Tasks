da0904f236d11cdde29b3aa1289b480f06d5ae2c
unknown <Kosarevis.74@gmail.com>
2023-03-23 14:57:35 +0500

    Написана 16 программа

---
+++ Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, по которой нужно взять два числа и определить
+// является ли одно число квадратом второго
+
+ Console.WriteLine("Введите первое число=");
+ int first = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("Введите второе число=");
+ int second = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Sqrt(first, second) || Sqrt(second, first) ? "Да" :"Нет");
+
+bool Sqrt (int first1, int second2)
+{
+
+    return first1 * first1 == second2;
+}
