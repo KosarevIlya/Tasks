eb6e061afd40787468a207926188176705bbb647
unknown <Kosarevis.74@gmail.com>
2023-03-23 12:59:49 +0500

    добавлена 11 задача, продолжаем тренировать методы

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программму, которая выводит трехзначное число и удаляет второе
+// 743 --> 73
+// 525 --> 55
+
+int number = new Random().Next (100, 1000);
+Console.WriteLine($"Случайное число из отрезка [100, 999] > {number}");
+
+
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine ($"Число из первой и второй цифры -->{maxDigit}");
+
+
+int MaxDigit(int num) // 
+{
+int firstDigit = num / 100;
+int secondDigit = num % 10;
+int result = firstDigit*10 + secondDigit;
+return result;
+}
