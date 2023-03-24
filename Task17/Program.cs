4958f5790e66f11d9a740b5447ad5a6524069f55
unknown <Kosarevis.74@gmail.com>
2023-03-24 19:18:32 +0500

    Создана программа 17

---
+++ Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Введите координаты точки =");
+Console.Write ("Х: ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Y: ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+int quater = Quater (xCoord, yCoord);
+string resault = quater > 0 
+? $"Указанные координаты соответсвуют {quater}" :
+"Введены неверные координаты";
+
+Console.WriteLine (resault);
+
+int Quater (int x, int y)
+{
+    if (x> 0 && y > 0) return 1;
+    if (x< 0 && y > 0) return 2;
+    if (x< 0 && y < 0) return 3;
+    if (x> 0 && y < 0) return 4;
+    return 0;
+}
