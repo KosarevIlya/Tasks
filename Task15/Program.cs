dbaf2a3074764b354a3a061b9d40f4aaa9b72cf6
unknown <Kosarevis.74@gmail.com>
2023-03-24 15:26:11 +0500

    Добавлено решение 15 задачи

---
+++ Task15/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Ведите трёхзначное число=");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (0 < number && number < 8)
+{
+Console .WriteLine (ThirdNumber(number));
+}
+else Console.WriteLine("Нет такого дня недели");
+
+bool ThirdNumber(int num)
+{
+     if (number > 5)
+
+     {
+        return true;
+     }
+      else{
+        return false;
+      }
+     
+
+}
