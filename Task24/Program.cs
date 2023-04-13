a14412cf1cca75c095aca3bcd96d6a449fad7b2b
unknown <Kosarevis.74@gmail.com>
2023-04-13 12:38:39 +0500

    Добалена 24 задача на посдсчет суммы всех целых чисел от 1 до введенного

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Введите натуральное число А, которое на выходе дает сумму всех натуральных чисел от 1 до А
+// 7--> 28
+// 4->10
+
+
+Console.WriteLine ("Введите значение числа А=");
+int pointA = Convert.ToInt32 (Console.ReadLine());
+
+int sumNumbers = SumNumbers(pointA);   
+Console.WriteLine ($" Сумма чисел = {sumNumbers}");
+int SumNumbers( int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    { 
+        sum += i;
+    }
+    return sum;
+
+}
+
