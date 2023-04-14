b13dc6088cd0c28479ae494429097a682b910dcc
unknown <Kosarevis.74@gmail.com>
2023-04-14 18:15:51 +0500

    Разбираемся с массивами, очень интересно, но ничего не понятно.Начиная с 30 задачи все с массивами

---
+++ Task32/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишити программу замены элементов массива: положительные элементы
+// замените на соответсвующие отрицательные и наобоброт
+// Например [3, -2, 4, -5]    [-3, 2, -4, 5]
+
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max+1);
+    }
+    return arr;
+
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i < arr.Length-1) Console.Write($"{arr[i]}, ");
+    else Console.Write ($"{arr[i]}");
+    
+}
+        Console.WriteLine("]");
+
+}
+
+    void ChancheSign (int [] arr)
+    {
+            for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = arr[i]*-1;
+            
+        }
+    }
+
+    int[] array = CreateArrayRndInt (12, -9, 9);
+    PrintArray(array);
+    ChancheSign(array);
+    PrintArray (array);
---
+++ Task33/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу по поиску заданного числа в массиве. пример
+// 4 -> [4, 5, 6, 6] --> да
+// 5 -> [7, 8, 9 ,10] - > нет
+
+Console.WriteLine("Введите число, которое хотите найти в массиве = ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max+1);
+    }
+    return arr;
+
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i < arr.Length-1) Console.Write($"{arr[i]}, ");
+    else Console.Write ($"{arr[i]}");
+    
+}
+        Console.WriteLine("]");
+
+}
+
+    bool FindeNumber (int [] arr, int num)
+    {
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == num) return true;
+                                   
+        }
+        return false;
+    }
+    int [] array = CreateArrayRndInt(12, -9, 9);
+    PrintArray(array);
+   string findeNumber = FindeNumber(array, number) ? "Число присутсвует в массиве" : "Число не присутсвует в массиве";
+    Console.WriteLine (findeNumber);
