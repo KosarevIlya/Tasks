b089834a0e2c64d5244b97ebafc4805fb18ec5bb
unknown <Kosarevis.74@gmail.com>
2023-04-15 18:58:41 +0500

    Решена 35 задача по поиску цифр из отрезка в массиве

---
+++ Task35/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 35. Создайте одномерный массив из 123 случайных чисел. Найдите колличество элементов 
+// массива, значения которых лежат в отрезке [10, 99]
+
+// создание массива и его заполнение
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max+1);
+    }
+    return arr;
+
+}
+
+int NumberInSegment (int [] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 9 && arr [i] < 100) sum++;
+                                   
+        }
+        return sum;
+    }
+        int [] array = CreateArrayRndInt(123, -0, 200);
+        int numberInSegment = NumberInSegment(array);
+
+        Console.WriteLine($"В заданном массиве число цифр из отрезка 10 - 99 {numberInSegment}");
